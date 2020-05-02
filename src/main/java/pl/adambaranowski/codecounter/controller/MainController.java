d228e827c470767ad1f998d9b20428fd383e1257
adambaranowski <adamb1066@gmail.com>
2020-05-02 17:18:13 +0200

    project structure

---
+++ src/main/java/pl/adambaranowski/codecounter/controller/MainController.java
@@ -0,0 +1,4 @@
+package pl.adambaranowski.codecounter.controller;
+
+public class MainController {
+}
---
+++ src/main/java/pl/adambaranowski/codecounter/main/GUIStarter.java
@@ -0,0 +1,9 @@
+package pl.adambaranowski.codecounter.main;
+
+
+
+public class GUIStarter {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
